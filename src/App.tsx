4ee16cc1322635e64d7ad3ebeefbf9d9f63d9c5e
SameedIlyas <silyas.bese22seecs@seecs.edu.pk>
2025-11-13 13:24:12 +0500

    fix: remove BrowserRouter import from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { AIFormProvider } from 'react-hook-form-ai';
 import Layout from './components/Layout';
 import Home from './pages/Home';
